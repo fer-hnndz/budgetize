8c9276fac6f1f2e018c7e1c74cd5e55aa553ecf4
fer-hnndz <jfernandohernandez28@gmail.com>
2024-01-12 17:31:45 -0600

    Added placeholder widgets to show monthly income/expense and recent transactions table on MainScreen

--- core/tui/screens/MainMenu.py
+++ core/tui/screens/MainMenu.py
@@ -1,7 +1,7 @@
 from textual.binding import Binding
-from textual.containers import Horizontal
+from textual.containers import Horizontal, Vertical
 from textual.screen import Screen
-from textual.widgets import Button, DataTable, Footer, Header, Label
+from textual.widgets import Button, DataTable, Footer, Header, Label, Rule, Tabs
 
 from core.accounts.account import Account
 from core.accounts.AccountType import AccountType
@@ -37,18 +37,44 @@ class MainMenu(Screen):
         self.app.sub_title = "Main Menu"
         yield Header()
         yield Footer()
-        yield DataTable()
+        yield Label("Accounts", id="accounts-label")
+        yield Rule(orientation="horizontal", line_style="heavy")
+        yield Horizontal(
+            DataTable(id="accounts-table"),
+            Vertical(
+                Label("Income this Month\n[green]L. 250.00[/green]"),
+                Label("Balance\n[green]L. 250.00[/green]"),
+                Label("Expenses this Month\n[red]L. 250.00[/red]"),
+            ),
+        )
         yield Horizontal(
             Button("Create Account", id="create-account-button"),
             Button("Manage Accounts"),
         )
+        yield Label("Recent Transactions", id="recent-transactions-label")
+
+        # Generate last 5 transactions
+        # TODO: Replace the place holders
+        yield DataTable(id="recent-transactions-table")
+
+        yield Rule(orientation="horizontal")
 
     def on_mount(self):
         self.user = load_user_data()
         self._update_account_tables()
+        self._update_recent_transactions_table()
+
+    def _update_recent_transactions_table(self) -> None:
+        # TODO: Implement this method based on users' transactions
+
+        table = self.get_widget_by_id("recent-transactions-table")
+        table.clear(columns=True)
+        table.add_columns("Account", "Amount", "Date", "Category")
+        table.add_row("Wallet", "[red] HNL 250.00", "2021-01-01", "Food")
+        table.add_row("Wallet", "[green] HNL 250.00", "2021-01-01", "Income")
 
     def _update_account_tables(self) -> None:
-        table = self.query_one(DataTable)
+        table = self.get_widget_by_id("accounts-table")
         table.clear(columns=True)
         table.add_columns("Account Name", "Account Type", "Balance", "Currency")
 
