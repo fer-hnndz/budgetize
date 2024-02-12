3bb19b099024dfa7bf02febee11ec23e70aae14e
fer-hnndz <jfernandohernandez28@gmail.com>
2024-02-12 10:24:20 -0600

    Add transaction details on ManageAccounts screen

--- budgetize/tui/screens/_manage_accounts.py
+++ budgetize/tui/screens/_manage_accounts.py
@@ -17,6 +17,7 @@ from textual.widgets import (
 )
 
 from budgetize.db import Database
+from budgetize.tui.modals import TransactionDetails
 
 
 class ManageAccounts(Screen):
@@ -72,10 +73,26 @@ class ManageAccounts(Screen):
             color = "[green]" if trans.amount > 0 else "[red]"
             date = Arrow.fromtimestamp(trans.timestamp).format("M/D/YYYY")
             table.add_row(
-                date, color + str(trans.amount), trans.category, trans.description
+                date,
+                color + str(trans.amount),
+                trans.category,
+                trans.description,
+                key=trans.id,
             )
 
         return table
+
+    def on_data_table_cell_selected(self, event: DataTable.CellSelected) -> None:
+        """Cell selection handler"""
+
+        selected_cell = event.coordinate.row
+        current_row = 0
+        for row in event.data_table.rows:
+            if current_row == selected_cell and row.value is not None:
+                details_screen = TransactionDetails(int(row.value))
+                self.app.push_screen(details_screen)
+                break
+            current_row += 1
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         """Button push handler"""
